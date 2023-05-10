b4a9e004c6e7410412e64d730041acf6d9a7d1a5
Elian jaramillo <jaramilloelian811@gmail.com>
2023-05-09 23:41:58 -0500

    frontend films

--- films/static/js/mediarandom.js
+++ films/static/js/mediarandom.js
@@ -2,15 +2,58 @@ const API_URL = 'http://127.0.0.1:8000/api/mediarandom/';
 
 function getMediaTemplate(media) {
 
-    tempstr = `<h3>  <p> Nombre: ${media.name}</p> <p> Genero: ${media.genre}</p> <p> Tipo: ${media.type}</p> <p> visualizaciones: ${media.views}</p> <p>promedio: ${media.average}</p> <button class="play-btn" id="vista" onclick="markAsViewed('${media.id}')"></button> </h3>`
+    tempstr = `<h3>  <p> Nombre: ${media.name}</p> <p> Genero: ${media.genre}</p> <p> Tipo: ${media.type}</p> <p> visualizaciones: ${media.views}</p> <p>promedio: ${media.average}</p>         <form>
+    <p class="clasificacion">
+      <input id="radio1" type="radio" name="estrellas" value="5"><!--
+      --><label for="radio1">★</label><!--
+      --><input id="radio2" type="radio" name="estrellas" value="4"><!--
+      --><label for="radio2">★</label><!--
+      --><input id="radio3" type="radio" name="estrellas" value="3"><!--
+      --><label for="radio3">★</label><!--
+      --><input id="radio4" type="radio" name="estrellas" value="2"><!--
+      --><label for="radio4">★</label><!--
+      --><input id="radio5" type="radio" name="estrellas" value="1"><!--
+      --><label for="radio5">★</label>
+    </p>
+  </form> <button href="javascript:location.reload()" class="play-btn" id="vista" onclick="markAsViewed('${media.id}')"></button> </h3>`
     return tempstr
 }
 
 function getMediaTemplateOrder(media) {
-    template = tempstr = `<h4  <p> Nombre: ${media.name}</p> <p> Genero: ${media.genre}</p> <p> Tipo: ${media.type}</p> <p> visualizaciones: ${media.views}</p> <p>promedio: ${media.average}</p> <button class="play-btn" id="vista" onclick="markAsViewed('${media.id}')"></button> </h3>`
+    template = tempstr = `<h4  <p> Nombre: ${media.name}</p> <p> Genero: ${media.genre}</p> <p> Tipo: ${media.type}</p> <p> visualizaciones: ${media.views}</p> <p>promedio: ${media.average}</p>         <form>
+    <p class="clasificacion">
+      <input id="radio1" type="radio" name="estrellas" value="5"><!--
+      --><label for="radio1">★</label><!--
+      --><input id="radio2" type="radio" name="estrellas" value="4"><!--
+      --><label for="radio2">★</label><!--
+      --><input id="radio3" type="radio" name="estrellas" value="3"><!--
+      --><label for="radio3">★</label><!--
+      --><input id="radio4" type="radio" name="estrellas" value="2"><!--
+      --><label for="radio4">★</label><!--
+      --><input id="radio5" type="radio" name="estrellas" value="1"><!--
+      --><label for="radio5">★</label>
+    </p>
+  </form> <button class="play-btn" id="vista" onclick="markAsViewed('${media.id}')"></button> </h3>`
     return template
 }
 
+function getMediaTemplateFilter(media) {
+    template = tempstr = `<h4  <p> Nombre: ${media.name}</p> <p> Genero: ${media.genre}</p> <p> Tipo: ${media.type}</p> <p> visualizaciones: ${media.views}</p> <p>promedio: ${media.average}</p>         <form>
+    <p class="clasificacion">
+      <input id="radio1" type="radio" name="estrellas" value="5"><!--
+      --><label for="radio1">★</label><!--
+      --><input id="radio2" type="radio" name="estrellas" value="4"><!--
+      --><label for="radio2">★</label><!--
+      --><input id="radio3" type="radio" name="estrellas" value="3"><!--
+      --><label for="radio3">★</label><!--
+      --><input id="radio4" type="radio" name="estrellas" value="2"><!--
+      --><label for="radio4">★</label><!--
+      --><input id="radio5" type="radio" name="estrellas" value="1"><!--
+      --><label for="radio5">★</label>
+    </p>
+  </form> <button class="play-btn" id="vista" onclick="markAsViewed('${media.id}')"></button> </h3>`
+    return template
+}
 const HTMLResponse = document.querySelector("#app")
 const tpl = document.createDocumentFragment()
 
@@ -76,7 +119,7 @@ function showMediasFilter(name, genre, type) {
         }))
         .then((response) => response.json())
         .then((users) => {
-            const tpl = users.map((media) => getMediaTemplate(media))
+            const tpl = users.map((media) => getMediaTemplateFilter(media))
             HTMLResponseFilter.innerHTML = `<p>${tpl}</p>`
         })
 }
@@ -99,7 +142,7 @@ function getCookie(name) {
 const csfr = document.getElementsByName("csrfmiddlewaretoken");
 async function markAsViewed(mediaIdSelected) {
     console.log(mediaIdSelected)
-
+    location.reload();
 
 
     const params = new URLSearchParams();
@@ -110,7 +153,4 @@ async function markAsViewed(mediaIdSelected) {
         body: params,
         headers: { "X-CSRFToken": csrftoken },
     })
-
-
-
 }
